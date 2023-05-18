8a027b3f34e5323b2e6814e01a1399503021042a
InfiniteXyy <xuyiyangwing@gmail.com>
2023-05-18 11:00:25 +0800

    chore: add flex-shrink for blink

--- src/views/chat/components/Message/Text.vue
+++ src/views/chat/components/Message/Text.vue
@@ -117,7 +117,7 @@ onUnmounted(() => {
       <div v-if="!inversion" class="flex items-end">
         <div v-if="!asRawText" class="w-full markdown-body" v-html="text" />
         <div v-else class="w-full whitespace-pre-wrap" v-text="text" />
-        <span v-if="loading" class="dark:text-white w-[4px] h-[20px] block animate-blink" />
+        <span v-if="loading" class="flex-shrink-0 dark:text-white w-[4px] h-[20px] block animate-blink" />
       </div>
       <div v-else class="whitespace-pre-wrap" v-text="text" />
     </div>
